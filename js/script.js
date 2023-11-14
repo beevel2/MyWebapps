3ce7152785b263214ec9dc2000398e20cfaaef10
beevel2 <rumaysa.azamatova03@gmail.com>
2023-11-14 10:39:32 +0300

    q

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 function purchase(item_id) {
-    alert('Вы купили товар ' + item_id)
+    const WebApp = window.Telegram.WebApp;
+    fetch(`http://127.0.0.1:8000/buy?user_id=${WebApp.WebAppUser.id}&item=${item_id}`)
+    .then(response => response.json())
+    .then(data => {
+        WebApp.showPopup({
+            title  : data.title,
+            message: data.message,
+            buttons: [{"type":"close"}]
+        })})
+    .catch(
+    WebApp.showPopup({
+        title  : "Ошибка",
+        message: "Произошла неизвестная ошибка",
+        buttons: [{"type":"close"}]
+        }))
 }
