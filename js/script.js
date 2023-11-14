20cfaee66a38251c6ba765d9cf404da5bd535917
Mert <139180336+beevel2@users.noreply.github.com>
2023-11-14 11:59:47 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 function purchase(item_id) {
-//    fetch(`http://127.0.0.1:8000/buy?user_id=${window.Telegram.WebApp.WebAppUser.id}&item=${item_id}`)
-//    .then(response => response.json())
-//    .then(data => {
-//        window.Telegram.WebApp.showPopup({
-//            title  : data.title,
-//            message: data.message,
-//            buttons: [{"type":"close"}]
-//        })})
-//    .catch(error => {
-//    window.Telegram.WebApp.showPopup({
-//        title  : "Ошибка",
-//        message: "Произошла неизвестная ошибка",
-//        buttons: [{"type":"close"}]
-//        })})
+   fetch(`http://127.0.0.1:8000/buy?user_id=${window.Telegram.WebApp.WebAppUser.id}&item=${item_id}`)
+   .then(response => response.json())
+   .then(data => {
+       window.Telegram.WebApp.showPopup({
+           title  : data.title,
+           message: data.message,
+           buttons: [{"type":"close"}]
+       })})
+   .catch(error => {
+   window.Telegram.WebApp.showPopup({
+       title  : "Ошибка",
+       message: "Произошла неизвестная ошибка",
+       buttons: [{"type":"close"}]
+       })})
     console.log(window.Telegram.WebApp)
 }
 
